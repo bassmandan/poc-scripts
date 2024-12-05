fa49cac6dedc595225e4695468286c18fd545e3d
bassmandan <daniel@thehardingfamily.org.uk>
2024-12-05 17:33:18 +0000

    Update domain-lookup.ps1

--- domain-lookup.ps1
+++ domain-lookup.ps1
@@ -2,5 +2,8 @@ $domain = "www.internetbadguys.com"
 $nslookupResult = nslookup $domain
 
 # Output the results of nslookup
-Write-Output "NSLookup results for $domain:"
+Write-Output "NSLookup results for ${domain}:"
 Write-Output $nslookupResult
+
+# Prevent the PowerShell window from closing immediately
+Read-Host -Prompt "Press Enter to exit"
